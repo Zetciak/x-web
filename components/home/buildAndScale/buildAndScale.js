49b8f8e98ab3edc17ca4d6061f7bade7845a48af
Zetciak <97193808+Zetciak@users.noreply.github.com>
2024-07-15 12:43:28 +0200

    xx

--- components/home/buildAndScale/buildAndScale.js
+++ components/home/buildAndScale/buildAndScale.js
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { Button, Typography } from '@mui/material';
 
 import bgImage from '@/public/images/buildAndScale/bgImage.png';
+import shadowBg from '@/public/images/buildAndScale/shadowBg.png';
 
 // >> Script
 export default function BuildAndScale(props) {
@@ -21,11 +22,23 @@ export default function BuildAndScale(props) {
 							className="image"
 						/>
 					</div>
+					<div className={styles.shadowBg}>
+						<Image
+							src={shadowBg}
+							alt=""
+							quality={99}
+							priority={true}
+							className="image"
+						/>
+					</div>
 					<div className={styles.stripesMask}>
 						<div className={styles.square} data-number="1"></div>
 						<div className={styles.square} data-number="2"></div>
 						<div className={styles.square} data-number="3"></div>
 						<div className={styles.square} data-number="4"></div>
+					</div>
+					<div className={styles.centerMask}>
+						<div className={styles.numbers}></div>
 					</div>
 				</div>
 				asd
